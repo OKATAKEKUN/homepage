bd9a19619345df63f263704dd59341aa0559fdfa
OKATAKEKUN <88257496+OKATAKEKUN@users.noreply.github.com>
2023-02-04 23:36:21 +0900

    写真をクリックした時、おかたけホームページに遷移しないように変更

--- pages/posts.js
+++ pages/posts.js
@@ -17,54 +17,44 @@ const posts = () => (
                         title="イングランドのリバプールで撮った写真。一眼レフで撮ったため、
                         雰囲気がよく映っていてお気に入りの写真です。"
                         thumbnail='/images/contents/liverpool.png'
-                        href="https://okatake.blog/"
                     />
                     <GridItem
                         title="イングランドのマンチェスターで撮った写真。こちらは高いビルが立ち並ぶ中心街で撮りました。
                         イングランドは各都市で独自の特徴があり、大変面白い。"
                         thumbnail='/images/contents/manchester.jpeg'
-                        href="https://okatake.blog/"
                     />
                     <GridItem
                         title="大学時代はサッカー部に所属していました。Aチームで公式戦には出たことなかったですが、
                         楽しくサッカーができました。"
                         thumbnail='/images/contents/football.jpeg'
-                        href="https://okatake.blog/"
                     />
                     <GridItem
                         title="バルセロナの街を歩いていたら急に出てきた巨大な建築物。サクラダファミリア。正直、ここまで大きいと思っていませんでした。"
                         thumbnail='/images/contents/sagradaFamilia.jpeg'
-                        href="https://okatake.blog/"
                     />
                     <GridItem
                         title="イタリアのローマといえばコロッセオ！！ここで争いが起こっていたのか？？よく知らんけど（笑）"
                         thumbnail='/images/contents/colosseum.jpeg'
-                        href="https://okatake.blog/"
                     />
                     <GridItem
                         title="イタリアのローマを歩いていた時、ここ見たことある！！と思い写真を撮った。あのローマの休日の有名なシーン。全部見たことないけど。。。"
                         thumbnail='/images/contents/romanHoliday.jpeg'
-                        href="https://okatake.blog/"
                     />
                     <GridItem
                         title="実際のコロッセオの中の写真です。なんか古代ローマに来たー！！って感じがしていい写真"
                         thumbnail='/images/contents/inside_colosseum.jpeg'
-                        href="https://okatake.blog/"
                     />
                     <GridItem
                         title="イングランド対クロアチアの試合を見に行きました。プレーがかっこいいモドリッチを見ることができて満足。ただ、少し距離が遠かった。。"
                         thumbnail='/images/contents/England_vs_Croatia.jpeg'
-                        href="https://okatake.blog/"
                     />
                     <GridItem
                         title="ロンドンのバッキンガム宮殿での一枚。当日は兵士？の行進などが見れました。"
                         thumbnail='/images/contents/Buckingham_Palace.jpeg'
-                        href="https://okatake.blog/"
                     />
                     <GridItem
                         title="小さいときの写真。なんか成長しちゃって悲しくなる。。こんなに小さかったのか。自分。"
                         thumbnail='/images/contents/fiveYearsOld.jpeg'
-                        href="https://okatake.blog/"
                     />
                 </SimpleGrid>
             </Section>
